de0af971c9826ed370b2178891c5f155d950fd62
ShabbirMinhas <shabbir.minhas@gmail.com>
2023-08-25 08:08:02 +0500

    git2.py updated by Minhas

--- git2.py
+++ git2.py
@@ -1,1 +1,2 @@
 new file git2.py is added to the local repo
+print('this change will be reverted')
--- stash.py
+++ stash.py
@@ -1,4 +1,3 @@
 print('stash is cool!')
 print('dont just simply learn it Master it')
-print("this change will be reverted back")
-print ('today 25 change will be reverted')
+print("changes to revet")
